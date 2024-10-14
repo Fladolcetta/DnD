188e930d645d75e793ef4c0aafa6e103838832c1
fladolcetta <Fladolcetta@bamboohr.com>
2024-10-14 13:50:19 -0500

    Add additional values

--- src/sheet_generator.py
+++ src/sheet_generator.py
@@ -10,14 +10,114 @@ class SheetGenerator:
         self.character = character
         self.key_pairs = {}
 
+
+    def unused_key_pairs(self) -> dict:
+        """ Generate key pairs for the character sheet. """
+        return {
+            "background": "Test",
+            "playername": "Test",
+            "alignment": "Test",
+            "experiencepoints": "Test",
+            "atkname1": "Test",
+            "atkbonus1": "Test",
+            "atkdamage1": "Test",
+            "atkname2": "Test",
+            "atkbonus2": "Test",
+            "atkdamage2": "Test",
+            "atkname3": "Test",
+            "atkbonus3": "Test",
+            "atkdamage3": "Test",
+            "cp": "Test",
+            "sp": "Test",
+            "ep": "Test",
+            "gp": "Test",
+            "pp": "Test"
+        }
+
+
     def generate_key_pairs(self) -> dict:
         """ Generate key pairs for the character sheet. """
-        text_printer = TextPrinter()
+        self.key_pairs = self.generate_basic_key_pairs()
+        self.key_pairs.update(self.generate_stat_key_pairs())
+        self.key_pairs.update(self.generate_saving_throw_key_pairs())
+        self.key_pairs.update(self.generate_skill_key_pairs())
+
+        return self.key_pairs
+
+    def generate_basic_key_pairs(self) -> dict:
+        """ Generate key pairs for the character sheet. """
         key_pairs = {
             "charname": self.character.name,
             "race": self.character.race,
             "classlevel": self.character.dnd_class + " " + str(self.character.level),
+            "proficiencybonus": self.character.proficiency_bonus,
+            "passiveperception": self.character.passive_perception,
+            "ac": self.character.ac,
+            "initiative": self.character.initiative,
+            "speed": self.character.speed,
+            "maxhp": self.character.hp,
+            "currenthp": self.character.hp,
+            "temphp": 0,
+            "totalhd": self.character.hit_die,
+            "remaininghd": self.character.hit_die.split("d")[0],
+            "otherprofs": str(self.character.languages),
+            "features": str(self.character.traits)
+        }
+        return key_pairs
+
+    def generate_stat_key_pairs(self) -> dict:
+        """ Generate key pairs for the character sheet. """
+        text_printer = TextPrinter()
+        key_pairs = {
             "Strengthscore": self.character.stats["Strength"],
             "Strengthmod": text_printer.print_modifiers(self.character.find_modifier_stat("Strength")),
+            "Dexterityscore": self.character.stats["Dexterity"],
+            "Dexteritymod": text_printer.print_modifiers(self.character.find_modifier_stat("Dexterity")),
+            "Constitutionscore": self.character.stats["Constitution"],
+            "Constitutionmod": text_printer.print_modifiers(self.character.find_modifier_stat("Constitution")),
+            "Wisdomscore": self.character.stats["Wisdom"],
+            "Wisdommod": text_printer.print_modifiers(self.character.find_modifier_stat("Wisdom")),
+            "Intelligencescore": self.character.stats["Intelligence"],
+            "Intelligencemod": text_printer.print_modifiers(self.character.find_modifier_stat("Intelligence")),
+            "Charismascore": self.character.stats["Charisma"],
+            "Charismamod": text_printer.print_modifiers(self.character.find_modifier_stat("Charisma"))
+        }
+        return key_pairs
+
+    def generate_saving_throw_key_pairs(self) -> dict:
+        """ Generate key pairs for the character sheet. """
+        text_printer = TextPrinter()
+        key_pairs = {
+            "Strengthsave": text_printer.print_modifiers(self.character.saving_throws["Strength"]),
+            "Dexteritysave": text_printer.print_modifiers(self.character.saving_throws["Dexterity"]),
+            "Constitutionsave": text_printer.print_modifiers(self.character.saving_throws["Constitution"]),
+            "Wisdomsave": text_printer.print_modifiers(self.character.saving_throws["Wisdom"]),
+            "Intelligencesave": text_printer.print_modifiers(self.character.saving_throws["Intelligence"]),
+            "Charismasave": text_printer.print_modifiers(self.character.saving_throws["Charisma"])
+        }
+        return key_pairs
+
+    def generate_skill_key_pairs(self) -> dict:
+        """ Generate key pairs for the character sheet. """
+        text_printer = TextPrinter()
+        key_pairs = {
+            "Acrobatics": text_printer.print_modifiers(self.character.all_skills["Acrobatics"]),
+            "AnimalHandling": text_printer.print_modifiers(self.character.all_skills["Animal Handling"]),
+            "Arcana": text_printer.print_modifiers(self.character.all_skills["Arcana"]),
+            "Athletics": text_printer.print_modifiers(self.character.all_skills["Athletics"]),
+            "Deception": text_printer.print_modifiers(self.character.all_skills["Deception"]),
+            "History": text_printer.print_modifiers(self.character.all_skills["History"]),
+            "Insight": text_printer.print_modifiers(self.character.all_skills["Insight"]),
+            "Intimidation": text_printer.print_modifiers(self.character.all_skills["Intimidation"]),
+            "Investigation": text_printer.print_modifiers(self.character.all_skills["Investigation"]),
+            "Medicine": text_printer.print_modifiers(self.character.all_skills["Medicine"]),
+            "Nature": text_printer.print_modifiers(self.character.all_skills["Nature"]),
+            "Perception": text_printer.print_modifiers(self.character.all_skills["Perception"]),
+            "Performance": text_printer.print_modifiers(self.character.all_skills["Performance"]),
+            "Persuasion": text_printer.print_modifiers(self.character.all_skills["Persuasion"]),
+            "Religion": text_printer.print_modifiers(self.character.all_skills["Religion"]),
+            "SleightofHand": text_printer.print_modifiers(self.character.all_skills["Sleight of Hand"]),
+            "Stealth": text_printer.print_modifiers(self.character.all_skills["Stealth"]),
+            "Survival": text_printer.print_modifiers(self.character.all_skills["Survival"])
         }
         return key_pairs
