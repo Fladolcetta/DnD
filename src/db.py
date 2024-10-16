1332908f44872d606e86d45d5a082616a6df6b8c
fladolcetta <Fladolcetta@bamboohr.com>
2024-10-16 09:16:58 -0500

    Working inserts!

--- src/db.py
+++ src/db.py
@@ -18,48 +18,26 @@ class DB:
     def insert_character(self, character: Character) -> int:
         """ Insert a character into the database. """
 
-        stats_sql = f"""
-                        INSERT INTO stats (
-                            dexterity,
-                            strength,
-                            constitution,
-                            intelligence,
-                            wisdom,
-                            charisma
-                            )
-                        VALUES (
-                            {character.stats["Dexterity"]},
-                            {character.stats["Strength"]},
-                            {character.stats["Constitution"]},
-                            {character.stats["Intelligence"]},
-                            {character.stats["Wisdom"]},
-                            {character.stats["Charisma"]}
-                            );
-                    """
-        stat_id = self.insert_into_table(stats_sql)
-        character_sql = f"""
-                            INSERT INTO character_data (
-                                char_name,
-                                dnd_class,
-                                dnd_race,
-                                stat_id
-                                )
-                            VALUES (
-                                {character.name},
-                                {character.dnd_class},
-                                {character.race},
-                                {stat_id}
-                                );
-                        """
-        character_id = self.insert_into_table(character_sql)
+        stats_sql = "INSERT INTO character_stats (dexterity, strength, constitution, intelligence, wisdom, charisma) VALUES ( %s, %s, %s, %s, %s, %s);"
+        stats_data = (character.stats["Dexterity"],
+                      character.stats["Strength"],
+                      character.stats["Constitution"],
+                      character.stats["Intelligence"],
+                      character.stats["Wisdom"],
+                      character.stats["Charisma"])
+        stat_id = self.insert_into_table(stats_sql, stats_data)
+        stat_id = 1
+        character_sql = "INSERT INTO character_data (char_name, dnd_class, dnd_race, stat_id) VALUES (%s, %s, %s, %s);"
+        character_data = (character.name, character.dnd_class, character.race, stat_id)
+        character_id = self.insert_into_table(character_sql, character_data)
         return character_id
 
-    def insert_into_table(self, sql: str) -> int:
+    def insert_into_table(self, sql: str, data: list) -> int:
         """ Insert into a table. """
         cursor = self.db.cursor(buffered=True)
-        use_sql = "USE characters;"
+        use_sql = "USE dnd;"
         cursor.execute(use_sql)
-        cursor.execute(sql)
+        cursor.execute(sql, data)
         last_id = "SELECT LAST_INSERT_ID();"
         cursor.execute(last_id)
         self.db.commit()
